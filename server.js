84b60e1bd63677fedca6680c0ccf836336807bc4
dzambani <dmzambani@gmail.com>
2021-01-19 00:39:47 -0800

    added formatting

--- server.js
+++ server.js
@@ -61,29 +61,29 @@ app.get('/signup', function (req, res, next) {
 app.post('/login', function(req, res, next) {
 
 	if (!req.body.username) {
-		return res.status(400).send('Username is required.');
+		return res.status(400).send('we need your name, baby.');
 	}
 
 	if (!req.body.password) {
-		return res.status(400).send('Password is required.');
+		return res.status(400).send('we need your password, baby.');
 	}
 
 	let baby_secrets = fs.readFileSync('./secret.json');
 	let json_baby_secrets = JSON.parse(baby_secrets);
 	let baby = json_baby_secrets.filter((x) => x.username === req.body.username);
 	if (baby.length === 0) {
-		return res.status(400).send('Invalid username.');
+		return res.status(400).send('whis baby doesn\'t exist! try another baby name.');
 	}
 
 	if (!bcrypt.compareSync(req.body.password, baby[0].password)) {
-		return res.status(400).send('Invalid password.');
+		return res.status(400).send('you entered the wrong password silly baby.');
     }
 
 	req.session.regenerate(function(error) {
 
 		if (error) {
 			console.log(error);
-			return res.status(500).send('An unexpected error occurred.');
+			return res.status(500).send('an unexpected error occurred.');
 		}
 
 		req.session.user = { name: req.body.username };
@@ -94,18 +94,18 @@ app.post('/login', function(req, res, next) {
 app.post('/signup', function (req, res, next) {
 
 	if (!req.body.username) {
-		return res.status(400).send('Username is required.');
+		return res.status(400).send('we need your name, baby.');
 	}
 
 	if (!req.body.password) {
-		return res.status(400).send('Password is required.');
+		return res.status(400).send('we need your password, baby.');
 	}
 
 	let baby_secrets = fs.readFileSync('./secret.json');
 	let json_baby_secrets = JSON.parse(baby_secrets);
 	let baby = json_baby_secrets.filter((x) => x.username === req.body.username);
 	if (baby.length !== 0) {
-		return res.status(400).send('That username has been taken, please pick a different one.');
+		return res.status(400).send('oopsie, this baby already exists, try another baby name.');
 	}
 
 	let bigbossbaby = json_baby_secrets.filter((x) => x.username === "bigbossbaby");
@@ -127,7 +127,7 @@ app.post('/signup', function (req, res, next) {
 
 		if (error) {
 			console.log(error);
-			return res.status(500).send('An unexpected error occurred.');
+			return res.status(500).send('an unexpected error occurred.');
 		}
 
 		req.session.user = { name: req.body.username };
@@ -177,47 +177,47 @@ app.post('/withdraw', requireLogin, function (req, res, next) {
 var transferFunds = function (to, from, start, amount, cb) {
 
 	if (!to) {
-		return cb(new Error('"To account" is required.'));
+		return cb(new Error('you need to say which baby you\'re sending to.'));
 	}
 
 	if (!from) {
-		return cb(new Error('"From account" is required.'));
+		return cb(new Error('you need to say which baby you\'re sending from.'));
 	}
 
 	let baby_secrets = fs.readFileSync('./secret.json');
 	let json_baby_secrets = JSON.parse(baby_secrets);
 	let baby_to = json_baby_secrets.filter((x) => x.username === to);
 	if (baby_to.length === 0) {
-		return cb(new Error('Cannot transfer funds to non-existent account ("' + to + '").'));
+		return cb(new Error('cannot transfer funds to non-existent baby ("' + to + '").'));
 	}
 
 	let baby_from = json_baby_secrets.filter((x) => x.username === from);
 	if (baby_from.length === 0) {
-		return cb(new Error('Cannot transfer funds from non-existent account ("' + from + '").'));
+		return cb(new Error('cannot transfer funds from non-existent baby ("' + from + '").'));
 	}
 
 	if (!start) {
-		return cb(new Error('"Start" is required.'));
+		return cb(new Error('looks like baby didn\'t say where they wanted to start.'));
 	}
 
 	if (!amount) {
-		return cb(new Error('"Amount" is required.'));
+		return cb(new Error('looks like baby didn\'t say how much they were sending.'));
 	}
 
 	if (to === from) {
-		return cb(new Error('You can\'t send to yourself!'));
+		return cb(new Error('you can\'t send to yourself silly baby!'));
 	}
 
 	try {
 		amount = new BigNumber(amount);
 	} catch (error) {
-		return cb(new Error('Amount must be a valid number.'));
+		return cb(new Error('baby! you can\'t count yet we know, but the amount must be a valid number.'));
 	}
 
 	try {
 		start = new BigNumber(start);
 	} catch (error) {
-		return cb(new Error('Start must be a valid number.'));
+		return cb(new Error('baby! you can\'t count yet we know, but the start must be a valid number.'));
 	}
 
 	if (!start.eq(1044)) {
@@ -250,24 +250,24 @@ var transferFunds = function (to, from, start, amount, cb) {
 var withdrawFunds = function (from, amount, cb) {
 
 	if (!from) {
-		return cb(new Error('"From account" is required.'));
+		return cb(new Error('you need to say which baby you\'re withdrawing from.'));
 	}
 
 	let baby_secrets = fs.readFileSync('./secret.json');
 	let json_baby_secrets = JSON.parse(baby_secrets);
 	let baby_from = json_baby_secrets.filter((x) => x.username === from);
 	if (baby_from.length === 0) {
-		return cb(new Error('Cannot transfer funds from non-existent account ("' + from + '").'));
+		return cb(new Error('cannot transfer funds from non-existent baby ("' + from + '").'));
 	}
 
 	if (!amount) {
-		return cb(new Error('"Amount" is required.'));
+		return cb(new Error('looks like baby didn\'t say how much they were withdrawing.'));
 	}
 
 	try {
 		amount = new BigNumber(amount);
 	} catch (error) {
-		return cb(new Error('Amount must be a valid number.'));
+		return cb(new Error('baby! you can\'t count yet we know, but the amount must be a valid number.'));
 	}
 
 	var cur_balance = new BigNumber(baby_from[0].balance.length);
